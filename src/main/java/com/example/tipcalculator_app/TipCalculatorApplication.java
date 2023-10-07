668239695f97e1e1c429980fe2559e819769a8ae
Amaurys De Los Santos <amaurysdlsm@gmail.com>
2023-10-07 19:39:47 -0400

    Changed The name of some files

--- src/main/java/com/example/tipcalculator_app/TipCalculatorApplication.java
+++ src/main/java/com/example/tipcalculator_app/TipCalculatorApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TipCalculatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(TipCalculatorApplication.class.getResource("TipCalculator-View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 820, 540);
+        stage.setTitle("Tip Calculator");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/tipcalculator_app/TipCalculatorController.java
+++ src/main/java/com/example/tipcalculator_app/TipCalculatorController.java
@@ -1,14 +1,46 @@
 package com.example.tipcalculator_app;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class TipCalculatorController {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField AmountaTextField;
+
+    @FXML
+    private Button CalculateButton;
+
+    @FXML
+    private Slider PercentSlider;
+
+    @FXML
+    private Label PercentageTextField;
+
+    @FXML
+    private TextField TipTextField;
+
+    @FXML
+    private TextField ToTalTextFiled;
+
+    @FXML
+    private void sliderMoved(MouseEvent event){
+        PercentageTextField.setText(String.valueOf(Math.round(PercentSlider.getValue())));
     }
+    @FXML
+    private void buttonPressed_CalculateTip(ActionEvent event) {
+        try{
+            System.out.println((Math.round(PercentSlider.getValue()) )/100.00);
+            TipTextField.setText(String.valueOf(Double.valueOf(AmountaTextField.getText())*( (Math.round (PercentSlider.getValue()) )/100.00 )));
+            ToTalTextFiled.setText(String.valueOf(Double.valueOf(AmountaTextField.getText()) + Double.valueOf(TipTextField.getText())));
+        }catch (NumberFormatException ex){
+            AmountaTextField.setText("Try Entering a Number");
+        }
+    }
+
 }
